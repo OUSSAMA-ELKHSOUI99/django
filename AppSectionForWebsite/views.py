53aea0a7880b31da27d09e62bc0f104d4f50e588
oussama ELkhsoui <oussamaelkhsoui99@gmail.com>
2025-07-26 17:38:59 +0100

    adding T

--- AppSectionForWebsite/views.py
+++ AppSectionForWebsite/views.py
@@ -5,7 +5,5 @@ from django.http import HttpResponse
 # we can have methods to send email, pull data from the database, transform data, etc.
 
 def say_hello(request):
-    """
-    A simple view that returns a greeting.
-    """
-    return HttpResponse("Hello, welcome to our website!")
+    # return HttpResponse("Hello, welcome to our website!")
+    return render(request, 'hello.html', {"name": "oussama"})  # Render a template with a greeting message
