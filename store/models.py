aaa7b79ee2e61dd212e4bdfa5f7a28ea4f95f116
oussama ELkhsoui <oussamaelkhsoui99@gmail.com>
2025-07-27 16:19:34 +0100

    More models

--- store/models.py
+++ store/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 # 
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
@@ -15,6 +20,8 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+    promotions = models.ManyToManyField(Promotion) # , related_name='products')
 
     def __str__(self):
         return self.name
@@ -70,7 +77,13 @@ class Adress(models.Model):
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
 
 class Cart(models.Model):
-    created_at = models.DateTimeField(auto_new_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    
 
 
 
